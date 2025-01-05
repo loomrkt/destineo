ddb483c5255fb2a3ecf62f04d39ded4b0df0afaf
RAKOTONDRAZAKA Nameno Fanantenana <mrnrakotondrazaka@gmail.com>
2025-01-05 18:25:20 +0000

    redaction des contenu landing page
    ajout des 5 languages et redaction jusqu'a la landing page

--- src/app/components/dropdown-language/dropdown-language.component.ts
+++ src/app/components/dropdown-language/dropdown-language.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -8,10 +8,29 @@ import { TranslateService } from '@ngx-translate/core';
   templateUrl: './dropdown-language.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DropdownLanguageComponent { 
+export class DropdownLanguageComponent implements OnInit{ 
   translate : TranslateService = inject(TranslateService);
+  ngOnInit(): void {
+    if(typeof localStorage != 'undefined')
+    {
+      const storedLanguage: string | null = localStorage.getItem('lang');
+      if(storedLanguage)
+      {
+        this.translate.use(storedLanguage);
+      }
+      else
+      {
+        this.translate.use('fr');
+      }
+    }
+  }
+
   translateText(lang : string)
   {
     this.translate.use(lang);
+    if(typeof localStorage != 'undefined')
+      {
+      localStorage.setItem('lang', lang);
+      }
   }
 }
--- src/app/layouts/layout/navbar/navbar.component.ts
+++ src/app/layouts/layout/navbar/navbar.component.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { DropdownLanguageComponent } from '../../../components/dropdown-language/dropdown-language.component';
 import { RouterLink } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [DropdownLanguageComponent, RouterLink],
+  imports: [DropdownLanguageComponent, RouterLink, TranslateModule],
   templateUrl: './navbar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
